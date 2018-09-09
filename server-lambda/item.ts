496fa3802dc53c02e5b762af7c3bc9551307fad8
ukon1990 <mail@jonaskf.net>
2018-09-09 09:18:31 +0200

    .

--- server-lambda/item.ts
+++ server-lambda/item.ts
@@ -1,3 +1,16 @@
+import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+const zlib = require('zlib');
+const request: any = require('request');
+const RequestPromise = require('request-promise');
 
+exports.handle = (event: APIGatewayEvent, context: Context, callback: Callback) => {
+  callback(null, event);
+};
 
-exports.get = () => {};
+export class ItemController {
+  public static get(): void {}
+
+  public static post(): void {}
+
+  public static patch(): void {}
+}
---
+++ server-lambda/util/gzip.util.ts
@@ -0,0 +1,27 @@
+
+import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+const zlib = require('zlib');
+
+export class Gzip {
+  public static compressResponse(context: Context, body, callback: Callback): void {
+    zlib.gzip(
+      new Buffer(
+        typeof body === 'string' ? body : JSON.stringify(body), 'utf-8'),
+      (err, ahData) => {
+        if (err) {
+          context.fail(err);
+        }
+        // context.succeed(response);
+        console.log('gzipped');
+        callback(null, {
+          statusCode: 200,
+          body: ahData.toString('base64'),
+          isBase64Encoded: true,
+          headers: {
+            'Content-Type': 'application/json',
+            'Content-Encoding': 'gzip'
+          }
+        });
+      });
+  }
+}
